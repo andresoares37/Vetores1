6607ac1f43736cd595ac015ee88cd86cefd7a4ee
Andre Soares <andresoares37@gmail.com>
2020-09-27 15:42:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vet = new double[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += vet[i];
+            }
+
+            double avg = sum / 3;
+
+            Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
